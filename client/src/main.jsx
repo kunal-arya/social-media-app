57a34f6d61904024fae32f11aa43bac35e0c7cce
kunal-arya <kunalaryad@gmail.com>
2023-04-26 17:16:31 +0530

    Register, Login Pages and Form created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import authReducer from "./state";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
   persistStore,
