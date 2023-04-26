2cac96e90672ad59778fe44e8806dadea19194a3
kunal-arya <kunalaryad@gmail.com>
2023-04-26 18:16:20 +0530

    UserWidget Created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "../navbar/index";
+import { useSelector } from "react-redux";
+import UserWidget from "../../scenes/widgets/userWidget";
 
 const HomePage = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreen ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+      </Box>
     </Box>
   );
 };
